dd4b8823e758d43feba718e75412d6aa4cf5b38c
Glsme <glasses.str.man@gmail.com>
2022-08-18 19:51:05 +0900

    Initial Commit

---
+++ YoutubeMusicLayout_example/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  YoutubeMusicLayout_example
+//
+//  Created by Seokjune Hong on 2022/08/18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
